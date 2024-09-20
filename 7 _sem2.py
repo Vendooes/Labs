539823ccfdf224a6046cf54f814075c7f74bae5c
Vendooes <folgop446@gmail.com>
2024-09-20 12:37:57 +0300

    First commit

--- 7 _sem2.py
+++ 7 _sem2.py
@@ -1,10 +1,12 @@
 a = input()
 w = []
-
 for i in range (9,0,-1):
     b = a.count(str(i))
     c = a.count(str(i-1))
     if b<c and b!=0:
         w.append(i-1)
-
+        for i in range (9,0,-1):
+            b = a.count(str(i))
+            if b>c:
+                w.append(i)
 print(w[-1])
---
+++ 8_sem2.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = input().split()
+p=[]
+for i in range (0,a-1):
+    for q in range (i+1,a-1):
+        if b[i]>b[q]:
+            b[i], b[q] = b[q],b[i]
+s = b[int((a-1)/2)]
+print(''.join(s))
