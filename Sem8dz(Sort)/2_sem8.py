fa883159ddb8f6e09c86b42cdf232b6162c8d26b
Vendooes <folgop446@gmail.com>
2024-11-08 18:27:43 +0300

    sem8

--- Sem8dz(Sort)/2_sem8.py
+++ Sem8dz(Sort)/2_sem8.py
@@ -1,32 +1,9 @@
-# seq = input().split()
-
-# def sort(seq):
-
-#     med = int((len(seq)-1)//2)
-#     medi = int(seq[med])
-#     if len(seq)<2:
-#         return seq
-#     else:
-#         mini = []
-#         ser = []
-#         big = []
-#         for i in seq:
-#             if int(i)<medi:
-#                 mini.append(int(i))
-#             if int(i) == medi:
-#                 ser.append(int(i))
-#             if int(i)>medi:
-#                 big.append(int(i))
-#         return sort(mini) + ser + sort(big)
-# print(sort(seq))
-
-seq = input("Введите числа через пробел: ").split()
+seq = input().split()
 
 def sort(seq):
     if len(seq) < 2:
         return seq
 
-    # Преобразуем строковые значения в целые числа
     seq = list(map(int, seq))
     
     med_index = (len(seq) - 1) // 2
