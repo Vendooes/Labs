a48747711da2ec47557cc9ec6b0f788be29979bb
Vendooes <folgop446@gmail.com>
2025-02-20 23:20:12 +0300

    DZ1

--- 2sem/Class/BFS.py
+++ 2sem/Class/BFS.py
@@ -1,19 +1,29 @@
-graph = {0: [1,4], 1: [2,3], 2:[3], 3:[0], 4:[1]}
+import numpy as np 
+
+graph = {0: [1,4], 1: [2,3], 2:[3], 3:[0], 4:[1], 5:[4]}
 
 def bfs(graph, start):
     visited = set()
     queue = []
 
+    dist = [np.inf for i in graph.keys()]
+    parent = [None for i in graph.keys()]
+
     visited.add(start)
     queue.append(start)
+    dist[start] = 0
 
     while queue:
         node = queue.pop(0)
-        print(node)
-        visited.add(node)
-        for neighbor in graph[node]:
-            if neighbor not in visited:
-                queue.append(neighbor)
-                visited.add(neighbor)
 
-bfs(graph,0)
+        for u in graph[node]:
+
+            if u not in visited:
+                visited.add(u)
+                queue.append(u)
+                dist[u] = dist[node] +1
+                parent[u] = node
+
+    return dist
+
+print(bfs(graph, 5))
